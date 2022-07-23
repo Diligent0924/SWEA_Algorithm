f961950656b7d9c0dc8ada3c41e82948c043fcbe
junior825 <57818015+junior825@users.noreply.github.com>
2022-07-23 23:35:02 +0900

    '??'

--- D2/1208_Flatten.py
+++ D2/1208_Flatten.py
@@ -1,5 +1,10 @@
 # 가장 높은 곳 => 가장 낮은 곳으로 옮기는 작업 수행
-for test_case in range(1, 2):
+for test_case in range(1, 11):
     N = int(input())
     list_a = list(map(int, input().split()))
+    count = 0
+    while N > count:
+        count += 1
+        list_a[list_a.index(max(list_a))] = list_a[list_a.index(max(list_a))] - 1
+        list_a[list_a.index(min(list_a))] = list_a[list_a.index(min(list_a))] + 1
     print(list_a)
